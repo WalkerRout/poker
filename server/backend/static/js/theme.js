4a4737862254fb9c80981d841e529662c21567fc
WalkerRout <walkerrout04@gmail.com>
2026-08-01 22:44:01 -0700

    chart height, fonts, remove generated static assets

--- server/backend/static/js/theme.js
+++
@@ -1,2 +0,0 @@
-var t=document.getElementById("theme-toggle"),e=document.documentElement;function n(){return e.classList.contains("dark")}function o(){t.textContent=n()?"\u2600\uFE0F":"\u{1F319}"}t.addEventListener("click",()=>{e.classList.toggle("dark"),localStorage.setItem("ui-schema-theme",n()?"dark":"light"),o()});o();
-//# sourceMappingURL=theme.js.map
